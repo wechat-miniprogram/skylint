79385483176d5abb7eced2fb0ef78b00b6c528a7
maniacata <maniaciachao@gmail.com>
2022-07-27 20:28:55 +0800

    build: update unbuild config

--- build.config.ts
+++ build.config.ts
@@ -2,29 +2,15 @@ import path from "path";
 import { defineBuildConfig } from "unbuild";
 
 export default defineBuildConfig({
-  // If entries is not provided, will be automatically inferred from package.json
-  entries: [
-    // default
-    "./src/index",
-    "./src/cli",
-    // mkdist builder transpiles file-to-file keeping original sources structure
-    // {
-    //   builder: "mkdist",
-    //   input: "./src/package/components/",
-    //   outDir: "./build/components",
-    // },
-  ],
+  entries: ["./src/index", "./src/cli"],
   rootDir: "./",
   rollup: {
     alias: {
       entries: {
-        "src": path.resolve("./src"),
+        src: path.resolve("./src"),
       },
     },
   },
-  // Change outDir, default is 'dist'
   outDir: "dist",
-
-  // Generates .d.ts declaration file
   declaration: true,
 });
