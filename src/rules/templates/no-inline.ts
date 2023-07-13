427a05ad937f83dfb4043b4c19f9155229be4232
maniacata <maniaciachao@gmail.com>
2023-07-13 21:18:44 +0800

    feat: update no-inline-text

--- src/rules/templates/no-inline.ts
+++ src/rules/templates/no-inline.ts
@@ -3,11 +3,20 @@ import { getLocationByNode } from "src/utils/dom-ast";
 import { isType } from "src/walker/html";
 import { defineRule, RuleType, RuleResultItem, RuleBasicInfoWithOptionalLevel } from "../interface";
 
+type Matcher = string | RegExp;
 interface BasicNoInlineCheckOptions {
   result: RuleResultItem;
   tagName?: string;
-  parentTagNameShouldBe?: string;
+  parentTagNameShouldBe?: Matcher | Matcher[];
 }
+const matchTagName = (tag: string, matcher?: Matcher | Matcher[]) => {
+  if (!matcher) return false;
+  if (Array.isArray(matcher)) {
+    return matcher.some((m) => tag.match(m));
+  } else {
+    return tag.match(matcher);
+  }
+};
 
 export const generateNoInlineTagChildrenCheck = (
   info: RuleBasicInfoWithOptionalLevel<RuleType.WXML>,
@@ -22,7 +31,7 @@ export const generateNoInlineTagChildrenCheck = (
         for (const child of node.childNodes) {
           const isText =
             (isType(child, "Text") && child.data.trim() !== "") || (isType(child, "Tag") && child.name === "text");
-          if (isText && isPrevText && (parentTagNameShouldBe ? node.name !== parentTagNameShouldBe : true)) {
+          if (isText && isPrevText && !matchTagName(node.name, parentTagNameShouldBe)) {
             const { start, end, path } = getLocationByNode(node);
             ctx.addResult({
               ...result,
